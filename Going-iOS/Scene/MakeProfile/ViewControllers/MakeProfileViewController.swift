6d2ba8baacb714bb4ad7e45d0afd745f684cbe55
0seoYun <yys020819@gmail.com>
2024-03-28 23:06:50 +0900

    [Fix] 네비 언더라인뷰 색상 변경 (#257)

--- Going-iOS/Scene/MakeProfile/ViewControllers/MakeProfileViewController.swift
+++ Going-iOS/Scene/MakeProfile/ViewControllers/MakeProfileViewController.swift
@@ -28,7 +28,7 @@ final class MakeProfileViewController: UIViewController {
     
     private let naviUnderLineView: UIView = {
         let view = UIView()
-        view.backgroundColor = .gray200
+        view.backgroundColor = .gray100
         return view
     }()
     
