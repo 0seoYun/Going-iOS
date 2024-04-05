e0ad3c11d89a5a6bae512d7cd7e1fb9e7c7a3922
0seoYun <yys020819@gmail.com>
2024-04-05 11:12:46 +0900

    [Fix] 여행생성완료뷰 - 라벨 센터 정렬 (#271)

--- Going-iOS/Scene/CreateTravel/ViewControllers/CreatingSuccessViewController.swift
+++ Going-iOS/Scene/CreateTravel/ViewControllers/CreatingSuccessViewController.swift
@@ -67,7 +67,9 @@ final class CreatingSuccessViewController: UIViewController {
     private let dDayLabel = DOOLabel(font: .pretendard(.detail2_bold), color: UIColor(resource: .red500))
   
     private let travelTitleLabel: DOOLabel = {
-        let label = DOOLabel(font: .pretendard(.head3), color: UIColor(resource: .gray700))
+        let label = DOOLabel(font: .pretendard(.head3), 
+                             color: UIColor(resource: .gray700),
+                             alignment: .center)
         label.lineBreakMode = .byTruncatingTail
         return label
     }()
